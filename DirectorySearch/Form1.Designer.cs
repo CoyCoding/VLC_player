3c94be836b013d28317a1370c6c1294ea7bb9034
matt coy <fluffytoycoy@gmail.com>
2018-11-11 19:36:04 -0600

    Add project files.

---
+++ DirectorySearch/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DirectorySearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.VideoPlayer = new AxAXVLC.AxVLCPlugin2();
+            this.Load_btn = new System.Windows.Forms.Button();
+            this.Stop_btn = new System.Windows.Forms.Button();
+            this.Pause_btn = new System.Windows.Forms.Button();
+            this.Play_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VideoPlayer
+            // 
+            this.VideoPlayer.Enabled = true;
+            this.VideoPlayer.Location = new System.Drawing.Point(12, 12);
+            this.VideoPlayer.Name = "VideoPlayer";
+            this.VideoPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("VideoPlayer.OcxState")));
+            this.VideoPlayer.Size = new System.Drawing.Size(776, 416);
+            this.VideoPlayer.TabIndex = 0;
+            // 
+            // Load_btn
+            // 
+            this.Load_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Load_btn.Location = new System.Drawing.Point(12, 434);
+            this.Load_btn.Name = "Load_btn";
+            this.Load_btn.Size = new System.Drawing.Size(152, 39);
+            this.Load_btn.TabIndex = 1;
+            this.Load_btn.Text = "Load";
+            this.Load_btn.UseVisualStyleBackColor = true;
+            this.Load_btn.Click += new System.EventHandler(this.Load_btn_Click);
+            // 
+            // Stop_btn
+            // 
+            this.Stop_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop_btn.Location = new System.Drawing.Point(636, 434);
+            this.Stop_btn.Name = "Stop_btn";
+            this.Stop_btn.Size = new System.Drawing.Size(152, 39);
+            this.Stop_btn.TabIndex = 1;
+            this.Stop_btn.Text = "Stop";
+            this.Stop_btn.UseVisualStyleBackColor = true;
+            this.Stop_btn.Click += new System.EventHandler(this.Stop_btn_Click);
+            // 
+            // Pause_btn
+            // 
+            this.Pause_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pause_btn.Location = new System.Drawing.Point(428, 434);
+            this.Pause_btn.Name = "Pause_btn";
+            this.Pause_btn.Size = new System.Drawing.Size(152, 39);
+            this.Pause_btn.TabIndex = 1;
+            this.Pause_btn.Text = "Pause";
+            this.Pause_btn.UseVisualStyleBackColor = true;
+            this.Pause_btn.Click += new System.EventHandler(this.Pause_btn_Click);
+            // 
+            // Play_btn
+            // 
+            this.Play_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Play_btn.Location = new System.Drawing.Point(220, 434);
+            this.Play_btn.Name = "Play_btn";
+            this.Play_btn.Size = new System.Drawing.Size(152, 39);
+            this.Play_btn.TabIndex = 1;
+            this.Play_btn.Text = "Play";
+            this.Play_btn.UseVisualStyleBackColor = true;
+            this.Play_btn.Click += new System.EventHandler(this.Play_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 477);
+            this.Controls.Add(this.Play_btn);
+            this.Controls.Add(this.Pause_btn);
+            this.Controls.Add(this.Stop_btn);
+            this.Controls.Add(this.Load_btn);
+            this.Controls.Add(this.VideoPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 VideoPlayer;
+        private System.Windows.Forms.Button Load_btn;
+        private System.Windows.Forms.Button Stop_btn;
+        private System.Windows.Forms.Button Pause_btn;
+        private System.Windows.Forms.Button Play_btn;
+    }
+}
+
---
+++ DirectorySearch/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectorySearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "All Files |*| .m4a |*.m4a";
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                VideoPlayer.playlist.add("file:///"+ openFile.FileName, openFile.SafeFileName, null);
+            }
+        }
+
+        private void Play_btn_Click(object sender, EventArgs e)
+        {
+            VideoPlayer.playlist.play();
+        }
+
+        private void Pause_btn_Click(object sender, EventArgs e)
+        {
+            VideoPlayer.playlist.pause();
+        }
+
+        private void Stop_btn_Click(object sender, EventArgs e)
+        {
+            VideoPlayer.playlist.stop();
+        }
+    }
+}
